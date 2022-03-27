c4087d0cc8e9f3f09a999adbe41d7d1e17f3400a
AltNeo <charismaticc27@gmail.com>
2022-03-28 00:45:14 +0530

    Fixed the compilation error the website gave me

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Lets Talk Code';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Lets Talk Code';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Something Witty To Enter';
 
   return {
     name,
